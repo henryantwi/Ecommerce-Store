61d611ca7e004512af4f852b6a6a3ec5878edb39
Henry Nana Antwi <henryantwi191@gmail.com>
2023-12-02 06:50:44 +0000

    paystack commit

--- orders/paystack.py
+++ orders/paystack.py
@@ -1,23 +1,25 @@
 from django.conf import settings
 import requests
 
+
+# noinspection PyPackageRequirements
 class Paystack:
-    PAYSTACK_SECRET_KEY = settings.PAYSTACK_SECRET_KEY    
+    PAYSTACK_SECRET_KEY = settings.PAYSTACK_SECRET_KEY
     base_url = 'https://api.paystack.co'
-    
+
     def verify_payment(self, ref, *args, **kwargs):
         path = f'/transaction/verify/{ref}'
-        
+
         headers = {
             "Authorization": f"Bearer {self.PAYSTACK_SECRET_KEY}",
             'Content-Type': 'application/json',
         }
         url = self.base_url + path
         response = requests.get(url, headers=headers)
-        
+
         if response.status_code == 200:
             response_data = response.json()
             return response_data['status'], response_data['data']
-        
+
         response_data = response.json()
         return response_data['status'], response_data["message"]

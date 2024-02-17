6eb5d78b5035b3396fa12d847757774d90823c85
henryantwi <henryantwi191@gmail.com>
2024-02-17 20:04:06 +0000

    Fix debug statement and update password reset page

--- core/settings.py
+++ core/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = config('DEBUG', default=False, cast=bool)
-ic(DEBUG)
+# ic(DEBUG)
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'nesttop.com']
 

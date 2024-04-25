b711df71c3f27d5666ccb375ba43b7bda3fffb02
henryantwi <henryantwi191@gmail.com>
2024-04-25 21:48:53 +0000

    settings added

--- core/settings.py
+++ core/settings.py
@@ -136,7 +136,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+# EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_USER = 'shop.techtroveteam@gmail.com'
 EMAIL_HOST_PASSWORD = 'bqomjjrqtpipahcu'
 
